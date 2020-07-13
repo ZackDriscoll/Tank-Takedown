8de852ca653e8f17d923ff3e588a6d223c81d92c
Zack Driscoll <zacdrisc@uat.edu>
2020-07-13 13:13:54 -0700

    Began working on Menus/UI

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //What happens when the player presses the Start Game Button
+        Debug.Log("[TitleMenu] Start Game");
+    }
+
+    public void OnClickOptions()
+    {
+        //What happens when the player presses the Options Button
+        Debug.Log("[TitleMenu] Options");
+    }
+
+    public void OnClickQuit()
+    {
+        //What happens when the player presses the Quit Button
+        Debug.Log("[TitleMenu] Quit Game");
+        Application.Quit();
+    }
+}
