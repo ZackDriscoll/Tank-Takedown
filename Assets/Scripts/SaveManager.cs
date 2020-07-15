42f41d186db9e34de7a2d586f29aeab8eeb6d96e
Zack Driscoll <zacdrisc@uat.edu>
2020-07-15 13:05:29 -0700

    Created SaveManager Script

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveManager : Singleton<SaveManager>
+{
+    public float musicVolume = 1.0f;
+    public float fxVolume = 1.0f;
+
+    public void Start()
+    {
+        Load();
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.SetFloat("fxVolume", fxVolume);
+        PlayerPrefs.Save();
+    }
+
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        }
+
+        if (PlayerPrefs.HasKey("fxVolume"))
+        {
+            fxVolume = PlayerPrefs.GetFloat("fxVolume");
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        Save();
+    }
+}
