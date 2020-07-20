fa575cdfb331cb16c5bcc5ef1aa6bf9defa4d1a4
Zack Driscoll <zacdrisc@uat.edu>
2020-07-20 13:22:44 -0700

    Created the basis of the high score system

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Vector3 offset;  
-
+    public Vector3 offset;
+    //public List<Camera> cameras;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position += offset;
+
+        /*cameras = GetComponents<Camera>();
+
+        if (cameras.Count == 1)
+        {
+            cameras[0].rect = new Rect(0, 0, 1, 1);
+        }
+        else
+        {
+            cameras[0].rect = new Rect(0, 0, 1, 0.5f);
+            cameras[0].rect = new Rect(0, 0.5f, 1, 0.5f);
+        }*/
     }
 
     // Update is called once per frame
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,27 +4,46 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    //Set variable for the high score list display size, 
+    //cannot change in code appart from initializing
+    private const int HIGHSCORETABLESIZE = 3;
+
+    //GameObjects for the player
     public GameObject playerPrefab;
     public GameObject player;
 
+    //Camera prefab to attatch to player
     public GameObject cameraPrefab;
 
+    //GameObjects and List to keep track of the enemies spawned
     public GameObject enemyPrefab;
     public GameObject enemy;
     public List<GameObject> enemies;
 
     public int demoNumber = 13;
+
+    //Lists to control the number of player spawn points, rooms, and scores
     public List<PlayerSpawnPoint> playerSpawnPoints;
     public List<Room> rooms;
+    public List<ScoreData> scores = new List<ScoreData>();
 
     protected override void Awake()
     {
         base.Awake();
         playerSpawnPoints = new List<PlayerSpawnPoint>();
+
+        //Sort the high score list from highest to lowest
+        scores.Sort();
+        scores.Reverse();
+
+        //Limit the size of the high score list
+        scores = scores.GetRange(index: 0, count: HIGHSCORETABLESIZE);
     }
 
     public void SpawnPlayer()
     {
+        //Spawn the player at a random spawn point within the generated map
+
         Debug.Log("Spawning Player");
 
         int playerNumber = Random.Range(0, rooms.Count);
@@ -40,6 +59,8 @@ public class GameManager : Singleton<GameManager>
 
     public void Respawn()
     {
+        //Respawn the player at a random spawn point if they die
+
         Debug.Log("First: " + player.transform.position);
 
         int playerNumber = Random.Range(0, rooms.Count);
@@ -55,6 +76,8 @@ public class GameManager : Singleton<GameManager>
 
     public void SpawnEnemies(Room room)
     {
+        //Spawn one enemy in each room and respawn them so there is always an enemy in each room
+
         enemy = Instantiate(enemyPrefab, room.enemySpawnPoint.position, Quaternion.identity);
 
         room.enemy = enemy;
